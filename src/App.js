0830fb3234a33ca27a85a72273d0ea8256c78450
tarieli-d <human6654@gmail.com>
2021-06-22 22:27:32 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className='main'>
+      <div className="product">
+          
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+          <div className='bottom'><button>Buy it</button></div>
+      </div>
+
+      <div className="product">
+    
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button></div>
+      </div> 
+
+      <div className="product">
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button></div>
+      </div> 
+      
+
+      <div className="product">
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button></div>
+      </div> 
+      
+
+      <div className="product">
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button></div>
+      </div> 
+ 
+
+      <div className="product">
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button>
+      </div>
+      </div> 
+      
+
+
+    </div>
+  );
+}
