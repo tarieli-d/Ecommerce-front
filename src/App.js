beb446f931f74bbf3d45612560888943e590b019
tarieli-d <human6654@gmail.com>
2021-06-22 22:45:47 +0400

    2 files modified,display clothes

--- src/App.js
+++ src/App.js
@@ -12,30 +12,30 @@ export default function App() {
 
       <div className="product">
     
-      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className="top"><img src="https://rukminim1.flixcart.com/image/714/857/jn4x47k0/shoe/k/q/s/023-black-6-6-lee-cargo-black-original-imaf9trhyzejgwvd.jpeg?q=50"></img></div>
       <div className='bottom'><button>Buy it</button></div>
       </div> 
 
       <div className="product">
-      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
-      <div className='bottom'><button>Buy it</button></div>
-      </div> 
-      
-
-      <div className="product">
-      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_6yc4bNq8EWnJIw6t3F56c3lo-5SoSH8G5w&usqp=CAU"></img></div>
       <div className='bottom'><button>Buy it</button></div>
       </div> 
       
 
       <div className="product">
-      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSG0C59pa3lhwJ7yZ5dkDbsCWzUwGWK7ueK1w&usqp=CAU"></img></div>
+      <div className='bottom'><button>Buy it</button></div>
+      </div> 
+      
+
+      <div className="product">
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWVXT5ywnZj2mosTqrtrm_t7jBSC2ZEeLg4g&usqp=CAU"></img></div>
       <div className='bottom'><button>Buy it</button></div>
       </div> 
  
 
       <div className="product">
-      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUC-_2I4_775RSeEIHfXH909ZeQw-7-fzucMuMqrI-O_GeIwZrIJwr9iyTCpTdOsc3_90&usqp=CAU"></img></div>
+      <div className="top"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSM-oYVY2SG9DJgM9TfpPt_ZuvtW2mUlQGRvg&usqp=CAU"></img></div>
       <div className='bottom'><button>Buy it</button>
       </div>
       </div> 
