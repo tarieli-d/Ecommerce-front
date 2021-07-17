b5640074a87320dc4685624d7d8dab8117a0f4e3
tarieli-d <human6654@gmail.com>
2021-07-17 04:29:40 +0400

    2 files modified--sliderbnb applied

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,62 @@
 import React, { useState } from 'react';
 import Slider from '@material-ui/core/Slider';
 import { useTranslation } from 'react-i18next';
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Slider from '@material-ui/core/Slider';
+
+const AirbnbSlider = withStyles({
+  root: {
+    color: '#3a8589',
+    height: 3,
+    padding: '13px 0'
+  },
+  thumb: {
+    height: 27,
+    width: 27,
+    backgroundColor: '#fff',
+    border: '1px solid currentColor',
+    marginTop: -12,
+    marginLeft: -13,
+    boxShadow: '#ebebeb 0 2px 2px',
+    '&:focus, &:hover, &$active': {
+      boxShadow: '#ccc 0 2px 3px 1px'
+    },
+    '& .bar': {
+      height: 9,
+      width: 1,
+      backgroundColor: 'currentColor',
+      marginLeft: 1,
+      marginRight: 1
+    }
+  },
+  track: {
+    height: 3
+  },
+  rail: {
+    color: '#d8d8d8',
+    opacity: 1,
+    height: 3
+  }
+})(Slider);
+
+function AirbnbThumbComponent(props) {
+  return (
+    <span {...props}>
+      <span className="bar" />
+      <span className="bar" />
+      <span className="bar" />
+    </span>
+  );
+}
 
 const Slideri = props => {
   const { t, i18n } = useTranslation();
   // Our States
-  const [value, setValue] = useState([0, 99]);
+  const [Value, setValue] = useState([0, 99]);
   // Changing State when volume increases/decreases
   const rangeSelector = (event, newValue) => {
+    console.log(newValue);
     setValue(newValue);
     props.rangeSelector(newValue);
   };
@@ -20,19 +69,29 @@ const Slideri = props => {
           margin: 'auto',
           display: 'block',
           width: '90%',
-          padding: '1rem',
+          padding: '.5rem',
           marginTop: '.3rem',
           border: '0.1rem solid rgb(196, 193, 193, 0.5)'
         }}
       >
-        <Slider
-          style={{}}
-          value={value}
+        <AirbnbSlider
+          value={Value}
           onChange={rangeSelector}
-          valueLabelDisplay="auto"
+          ThumbComponent={AirbnbThumbComponent}
         />
         <p>
-          {value[0]}-დან / {value[1]}-მდე{' '}
+          <input
+            onChange={e => {
+              setValue([e.currentTarget.value, Value[1]]);
+            }}
+            value={Value[0]}
+          />
+          <input
+            onChange={e => {
+              setValue([Value[0], e.currentTarget.value]);
+            }}
+            value={Value[1]}
+          />{' '}
         </p>
       </div>
     </>
